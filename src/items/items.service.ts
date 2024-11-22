cec2a01e3d1a0c9179dd25f59c05e2dc79bf3235
amontalvof <amontalvof92@gmail.com>
2024-11-21 21:16:51 -0500

    feat: Create items - Service and DTOs

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { CreateItemInput } from './dto/create-item.input';
-import { UpdateItemInput } from './dto/update-item.input';
+import { CreateItemInput } from './dto/inputs/create-item.input';
+import { UpdateItemInput } from './dto/inputs/update-item.input';
+import { Item } from './entities/item.entity';
 
 @Injectable()
 export class ItemsService {
-  create(createItemInput: CreateItemInput) {
-    return 'This action adds a new item';
-  }
+    create(createItemInput: CreateItemInput): Promise<Item> {
+        return 'This action adds a new item';
+    }
 
-  findAll() {
-    return `This action returns all items`;
-  }
+    findAll() {
+        return `This action returns all items`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} item`;
+    }
 
-  update(id: number, updateItemInput: UpdateItemInput) {
-    return `This action updates a #${id} item`;
-  }
+    update(id: number, updateItemInput: UpdateItemInput) {
+        return `This action updates a #${id} item`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} item`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} item`;
+    }
 }
