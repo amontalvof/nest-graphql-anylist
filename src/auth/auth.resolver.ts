cef05ca1e44125d246c35f32437ebf6dece6789b
amontalvof <amontalvof92@gmail.com>
2024-12-03 09:56:43 -0500

    feat: Types, Inputs and ObjectTypes

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,23 +1,26 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
+import { SignUpInput } from './dto/inputs/signup.input';
+import { AuthResponse } from './types/auth-response.types';
 
 @Resolver()
 export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {}
 
-  @Mutation(,{name:'signup'})
-    async signUp(@Args('input') input: SignUpInput) {
-        // return this.authService.signUp(input);
+    @Mutation(() => AuthResponse, { name: 'signup' })
+    async signUp(
+        @Args('signupInput') signupInput: SignUpInput
+    ): Promise<AuthResponse> {
+        return this.authService.signUp(signupInput);
     }
 
-    @Mutation(,{name:'login'})
-    async login(@Args('input') input: LoginInput) {
-        // return this.authService.login(input);
-    }
+    // @Mutation(,{name:'login'})
+    // async login(@Args('input') input: LoginInput) {
+    //     // return this.authService.login(input);
+    // }
 
-    @Query(,{name:'revalidateToken'})
-    async revalidateToken(@Args('token') token: string) {
-        // return this.authService.revalidateToken(token);
-    }
-
+    // @Query(,{name:'revalidateToken'})
+    // async revalidateToken(@Args('token') token: string) {
+    //     // return this.authService.revalidateToken(token);
+    // }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { SignUpInput } from './dto/inputs/signup.input';
+import { AuthResponse } from './types/auth-response.types';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    async signUp(signupInput: SignUpInput): Promise<AuthResponse> {
+        console.log({ signupInput });
+        throw new Error('Method not implemented.');
+    }
+}
