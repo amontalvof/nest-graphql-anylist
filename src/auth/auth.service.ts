e6f16eb47d2763e39201dec132c8b610c91571b9
amontalvof <amontalvof92@gmail.com>
2024-12-05 20:52:33 -0500

    feat: Create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { SignUpInput } from './dto/inputs/signup.input';
 import { AuthResponse } from './types/auth-response.types';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
+    constructor(private readonly usersService: UsersService) {}
     async signUp(signupInput: SignUpInput): Promise<AuthResponse> {
-        console.log({ signupInput });
-        throw new Error('Method not implemented.');
+        const user = await this.usersService.create(signupInput);
+        return {
+            user,
+            token: 'ABC123',
+        };
     }
 }
