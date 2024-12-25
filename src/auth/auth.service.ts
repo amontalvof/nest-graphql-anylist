38c2bde9d816ae8db38f2b4acc95e3700700725a
amontalvof <amontalvof92@gmail.com>
2024-12-25 18:17:54 -0500

    feat: Generate our JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,28 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { SignUpInput } from './dto/inputs/signup.input';
 import { AuthResponse } from './types/auth-response.types';
 import { UsersService } from 'src/users/users.service';
 import { LoginInput } from './dto/inputs/login.input';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService
+    ) {}
+
+    private getJwtToken(userId: string): string {
+        return this.jwtService.sign({ id: userId });
+    }
+
     async signUp(signupInput: SignUpInput): Promise<AuthResponse> {
         const user = await this.usersService.create(signupInput);
+        const token = this.getJwtToken(user.id);
         return {
             user,
-            token: 'ABC123',
+            token,
         };
     }
 
@@ -22,9 +32,10 @@ export class AuthService {
         if (!bcrypt.compareSync(password, user.password)) {
             throw new BadRequestException('Invalid credentials');
         }
+        const token = this.getJwtToken(user.id);
         return {
             user,
-            token: 'ABC123',
+            token,
         };
     }
 }
