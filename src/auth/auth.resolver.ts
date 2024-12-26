3bb5482eccdc52eeaa1ead21f9a1135ab2d66e42
amontalvof <amontalvof92@gmail.com>
2024-12-26 09:05:03 -0500

    feat: JwtAuthGuard

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,8 +1,10 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { SignUpInput } from './dto/inputs/signup.input';
 import { AuthResponse } from './types/auth-response.types';
 import { LoginInput } from './dto/inputs/login.input';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Resolver()
 export class AuthResolver {
@@ -22,8 +24,11 @@ export class AuthResolver {
         return this.authService.login(loginInput);
     }
 
-    // @Query(,{name:'revalidateToken'})
-    // async revalidateToken(@Args('token') token: string) {
-    //     // return this.authService.revalidateToken(token);
-    // }
+    @Query(() => AuthResponse, { name: 'revalidateToken' })
+    @UseGuards(JwtAuthGuard)
+    revalidateToken() // @CurrentUser() user: User,
+    : AuthResponse {
+        // return this.authService.revalidateToken(token);
+        throw new Error('Not implemented');
+    }
 }
